a0ca2adb0b50bb4bc103160745d2ed867729dfba
Kryskar <132471940+Kryskar@users.noreply.github.com>
2024-05-06 19:37:08 +0200

    fix: add type

---
+++ react-big-calendar.d.ts
@@ -0,0 +1,10 @@
+interface Event {
+    allDay?: boolean | undefined;
+    description?: undefined | string;
+    end?: Date | undefined;
+    id?: string;
+    // eslint-disable-next-line
+    resource?: any;
+    start?: Date | undefined;
+    title?: undefined | string;
+}
